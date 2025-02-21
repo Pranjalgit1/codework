a761549fbf56b4cbbe03699c445d0c23aa62f7c7
Pranjal Chamoli <154313335+Pranjalgit1@users.noreply.github.com>
2025-02-21 09:50:49 +0530

    Add files via upload

---
+++ java/subarrays.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class subarrays {
+    public static void main(String[] args) {
+        int number[]={1,2,3,4,5,6};
+        
+        printsubarrays(number);
+
+       
+    }
+
+    public static void printsubarrays(int number[]) {
+        for(int i=0;i<number.length;i++) {
+            int start=i;
+            for(int j=i+1;j<number.length;j++) {
+                int end=j;
+                for(int k = start;k<=end;k++){
+                    System.out.print(number[k]+" ");
+
+                }
+                System.out.println();
+
+            }
+         
+        }
+      
+    
+    }
+}
